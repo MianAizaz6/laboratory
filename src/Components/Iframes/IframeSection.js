5a2b13357e5b11664734641ec72934aa1853a2b8
Mian Aizaz <aizazalishah31@gmail.com>
2022-03-15 23:12:25 +0500

    cat section added

--- src/Components/Iframes/IframeSection.js
+++ src/Components/Iframes/IframeSection.js
@@ -4,10 +4,10 @@ import IframeComponent from './IframeComponent'
 function IframeSection() {
   return (
     <div className='row gx-0'>
-        <div className='col-lg-6 p-0'>
+        <div className='col-lg-6 col-md-6 p-0'>
             <IframeComponent source={"https://www.youtube.com/embed/X8ipUgXH6jw"} title={"Twitter"}/>
         </div>
-        <div className='col-lg-6 p-0'>
+        <div className='col-lg-6 col-md-6 p-0'>
         <IframeComponent source={"https://www.youtube.com/embed/X8ipUgXH6jw"} title={"Facebook"}/>
         </div>
     </div>
