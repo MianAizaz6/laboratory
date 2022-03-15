1a5ce4e15a954e61d3008232149b1796a6233016
muhammadumair11 <mu.umair11@gmail.com>
2022-03-15 11:50:31 +0500

    major-cleanup-&-changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-
-import './App.css';
 import TopSection from './Components/TopSection';
 import Header from './Components/Header';
 
 function App() {
   return (
-    <div >
+    <>
        <TopSection />
        <Header />
-    </div>
+    </>
   );
 }
 
