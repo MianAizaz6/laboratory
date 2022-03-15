7488a3a0691f7d4cdab4265955fdb7de137edb6a
Mian Aizaz <aizazalishah31@gmail.com>
2022-03-15 11:24:41 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TopSection from './Components/TopSection';
+import Header from './Components/Header';
+
+function App() {
+  return (
+    <div >
+       <TopSection />
+       <Header />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TopSection.js
@@ -0,0 +1,58 @@
+import React,{Fragment} from 'react';
+import '../Styles/TopSection.css';
+import Logo from '../Images/csir.png'
+import Logo1 from '../Images/img-a.png'
+
+function TopSection() {
+  return (
+      <Fragment>
+        <div className="container-fluid bg-grey">
+            <div className="container">
+                    <div className="row">
+                            <div className="col-md-6 ms-auto col-sm-10">
+                                <ul className="top__section mb-0">
+                                    <li>Employer Conrner</li>
+                                    <li>Screen Reader Access</li>
+                                    <li><i class="fas fa-share-square"></i></li>
+                                    <li><i class="fas fa-share-square"></i></li>
+                                    <li><i class="fas fa-share-square"></i></li>
+                                    <li><i class="fas fa-share-square"></i></li>
+
+                                </ul>
+                            </div>
+                            
+                    </div>
+                    
+            </div>
+        </div>
+        <div className="container-fluid">
+            <div className="container">
+                   
+                    <div className="row p-2 d-flex justify-content-center">
+                        <div className="col-md-6 sm-12 lg-6">
+                            <div className="logo-section">
+                                <img src={Logo} alt="" />
+                                <p>
+                                    COUNCIL OF SCIENTIFIC & INDUSTRIAL RESEARCH
+                                    MINISTRY OF SCIENCE & TECHNOLOGY, GOVT. OF INDIA</p>
+                            </div>
+                        </div>
+                        <div className="col-md-6 sm-12 md-6">
+                            <div className="logos">
+                                <a href="#"><img src={Logo1} alt="Logo" /></a>
+                                <a href="#"><img src={Logo1} alt="Logo" /></a>
+                                <a href="#"><img src={Logo1} alt="Logo" /></a>
+                                <a href="#"><img src={Logo1} alt="Logo" /></a>
+                                <a href="#"><img src={Logo1} alt="Logo" /></a>
+                                
+                            </div>
+                        </div>
+                    </div>
+                    
+            </div>
+        </div>
+    </Fragment>
+  )
+}
+
+export default TopSection
